b21e724b5d061bdcde4dd0c6a38963ef729a8573
iamrob <rob.duits@incentro.com>
2022-12-22 13:19:28 +0100

    Add close option

---
+++ dist/tmp/widgets/PWAOverlayForiOS.editorConfig.js
@@ -0,0 +1,355 @@
+'use strict';
+
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0) { ; } } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i.return && (_r = _i.return(), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+var dist = {};
+var common$2 = {};
+Object.defineProperty(common$2, "__esModule", {
+  value: true
+});
+common$2.ensure = void 0;
+function ensure(arg) {
+  if (arg == null) {
+    throw new Error("Did not expect an argument to be undefined");
+  }
+  return arg;
+}
+common$2.ensure = ensure;
+var common$1 = {};
+Object.defineProperty(common$1, "__esModule", {
+  value: true
+});
+common$1.extractStyles = common$1.mergeNativeStyles = void 0;
+function mergeNativeStyles(defaultStyle, overrideStyles) {
+  var styles = [defaultStyle].concat(_toConsumableArray(overrideStyles.filter(function (object) {
+    return object !== undefined;
+  })));
+  return Object.keys(defaultStyle).reduce(function (flattened, currentKey) {
+    var styleItems = styles.map(function (object) {
+      return object[currentKey];
+    });
+    return Object.assign(Object.assign({}, flattened), _defineProperty({}, currentKey, flattenObjects(styleItems)));
+  }, {});
+}
+common$1.mergeNativeStyles = mergeNativeStyles;
+function flattenObjects(objects) {
+  return objects.reduce(function (merged, object) {
+    return Object.assign(Object.assign({}, merged), object);
+  }, {});
+}
+function extractStyles(source, extractionKeys) {
+  if (!source) {
+    return [{}, {}];
+  }
+  return Object.entries(source).reduce(function (_ref, _ref2) {
+    var _ref3 = _slicedToArray(_ref, 2),
+      extracted = _ref3[0],
+      rest = _ref3[1];
+    var _ref4 = _slicedToArray(_ref2, 2),
+      key = _ref4[0],
+      value = _ref4[1];
+    if (extractionKeys.includes(key)) {
+      extracted[key] = value;
+    } else {
+      rest[key] = value;
+    }
+    return [extracted, rest];
+  }, [{}, {}]);
+}
+common$1.extractStyles = extractStyles;
+var common = {};
+Object.defineProperty(common, "__esModule", {
+  value: true
+});
+common.parseInlineStyle = void 0;
+function parseInlineStyle() {
+  var style = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "";
+  try {
+    return style.split(";").reduce(function (styleObject, line) {
+      var pair = line.split(":");
+      if (pair.length === 2) {
+        var name = pair[0].trim().replace(/(-.)/g, function (match) {
+          return match[1].toUpperCase();
+        });
+        styleObject[name] = pair[1].trim();
+      }
+      return styleObject;
+    }, {});
+  } catch (_) {
+    return {};
+  }
+}
+common.parseInlineStyle = parseInlineStyle;
+var typings = {};
+var PageEditor = {};
+Object.defineProperty(PageEditor, "__esModule", {
+  value: true
+});
+(function (exports) {
+  var __createBinding = commonjsGlobal && commonjsGlobal.__createBinding || (Object.create ? function (o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, {
+      enumerable: true,
+      get: function get() {
+        return m[k];
+      }
+    });
+  } : function (o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+  });
+  var __exportStar = commonjsGlobal && commonjsGlobal.__exportStar || function (m, exports) {
+    for (var p in m) {
+      if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+    }
+  };
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  __exportStar(PageEditor, exports);
+})(typings);
+var utils = {};
+var PageEditorUtils = {};
+Object.defineProperty(PageEditorUtils, "__esModule", {
+  value: true
+});
+PageEditorUtils.moveProperty = PageEditorUtils.transformGroupsIntoTabs = PageEditorUtils.changePropertyIn = PageEditorUtils.hideNestedPropertiesIn = PageEditorUtils.hidePropertiesIn = PageEditorUtils.hidePropertyIn = void 0;
+function hidePropertyIn(propertyGroups, _value, key, nestedPropIndex, nestedPropKey) {
+  modifyProperty(function (_, index, container) {
+    return container.splice(index, 1);
+  }, propertyGroups, key, nestedPropIndex, nestedPropKey);
+}
+PageEditorUtils.hidePropertyIn = hidePropertyIn;
+function hidePropertiesIn(propertyGroups, _value, keys) {
+  keys.forEach(function (key) {
+    return modifyProperty(function (_, index, container) {
+      return container.splice(index, 1);
+    }, propertyGroups, key, undefined, undefined);
+  });
+}
+PageEditorUtils.hidePropertiesIn = hidePropertiesIn;
+function hideNestedPropertiesIn(propertyGroups, _value, key, nestedPropIndex, nestedPropKeys) {
+  nestedPropKeys.forEach(function (nestedKey) {
+    return hidePropertyIn(propertyGroups, _value, key, nestedPropIndex, nestedKey);
+  });
+}
+PageEditorUtils.hideNestedPropertiesIn = hideNestedPropertiesIn;
+function changePropertyIn(propertyGroups, _value, modify, key, nestedPropIndex, nestedPropKey) {
+  modifyProperty(modify, propertyGroups, key, nestedPropIndex, nestedPropKey);
+}
+PageEditorUtils.changePropertyIn = changePropertyIn;
+function transformGroupsIntoTabs(properties) {
+  var groups = [];
+  properties.forEach(function (property) {
+    if (property.propertyGroups) {
+      groups.push.apply(groups, _toConsumableArray(property.propertyGroups));
+      property.propertyGroups = [];
+    }
+  });
+  properties.push.apply(properties, groups);
+}
+PageEditorUtils.transformGroupsIntoTabs = transformGroupsIntoTabs;
+function modifyProperty(modify, propertyGroups, key, nestedPropIndex, nestedPropKey) {
+  propertyGroups.forEach(function (propGroup) {
+    var _a;
+    if (propGroup.propertyGroups) {
+      modifyProperty(modify, propGroup.propertyGroups, key, nestedPropIndex, nestedPropKey);
+    }
+    (_a = propGroup.properties) === null || _a === void 0 ? void 0 : _a.forEach(function (prop, index, array) {
+      if (prop.key === key) {
+        if (nestedPropIndex === undefined || nestedPropKey === undefined) {
+          modify(prop, index, array);
+        } else if (prop.objects) {
+          modifyProperty(modify, prop.objects[nestedPropIndex].properties, nestedPropKey);
+        } else if (prop.properties) {
+          modifyProperty(modify, prop.properties[nestedPropIndex], nestedPropKey);
+        }
+      }
+    });
+  });
+}
+function moveProperty(fromIndex, toIndex, properties) {
+  if (fromIndex >= 0 && toIndex >= 0 && fromIndex < properties.length && toIndex < properties.length && fromIndex !== toIndex) {
+    properties.splice.apply(properties, [toIndex, 0].concat(_toConsumableArray(properties.splice(fromIndex, 1))));
+  }
+}
+PageEditorUtils.moveProperty = moveProperty;
+(function (exports) {
+  var __createBinding = commonjsGlobal && commonjsGlobal.__createBinding || (Object.create ? function (o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, {
+      enumerable: true,
+      get: function get() {
+        return m[k];
+      }
+    });
+  } : function (o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+  });
+  var __exportStar = commonjsGlobal && commonjsGlobal.__exportStar || function (m, exports) {
+    for (var p in m) {
+      if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+    }
+  };
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  __exportStar(PageEditorUtils, exports);
+})(utils);
+(function (exports) {
+  var __createBinding = commonjsGlobal && commonjsGlobal.__createBinding || (Object.create ? function (o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, {
+      enumerable: true,
+      get: function get() {
+        return m[k];
+      }
+    });
+  } : function (o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+  });
+  var __exportStar = commonjsGlobal && commonjsGlobal.__exportStar || function (m, exports) {
+    for (var p in m) {
+      if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+    }
+  };
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  __exportStar(common$2, exports);
+  __exportStar(common$1, exports);
+  __exportStar(common, exports);
+  __exportStar(typings, exports);
+  __exportStar(utils, exports);
+})(dist);
+
+/**
+ * @typedef Property
+ * @type {object}
+ * @property {string} key
+ * @property {string} caption
+ * @property {string} description
+ * @property {string[]} objectHeaders
+ * @property {ObjectProperties[]} objects
+ * @property {Properties[]} properties
+ */
+
+/**
+ * @typedef ObjectProperties
+ * @type {object}
+ * @property {PropertyGroup[]} properties
+ * @property {string[]} captions
+ */
+
+/**
+ * @typedef PropertyGroup
+ * @type {object}
+ * @property {string} caption
+ * @property {PropertyGroup[]} propertyGroups
+ * @property {Property[]} properties
+ */
+
+/**
+ * @typedef Properties
+ * @type {PropertyGroup}
+ */
+
+/**
+ * @typedef Problem
+ * @type {object}
+ * @property {string} property
+ * @property {("error" | "warning" | "deprecation")} severity
+ * @property {string} message
+ * @property {string} studioMessage
+ * @property {string} url
+ * @property {string} studioUrl
+ */
+
+/**
+ * @param {object} values
+ * @param {Properties} defaultProperties
+ * @param {("web"|"desktop")} target
+ * @returns {Properties}
+ */
+
+// export function getProperties(values, defaultProperties, target) {
+// Do the values manipulation here to control the visibility of properties in Studio and Studio Pro conditionally.
+/* Example
+if (values.myProperty === "custom") {
+    delete defaultProperties.properties.myOtherProperty;
+}
+*/
+// }
+
+function getProperties(values, defaultProperties) {
+  var propsToHide = [];
+  if (values.instructionCanBeClosed === false) {
+    propsToHide.push("instructionClosed");
+    // delete defaultProperties.properties.instructionClosed;
+  }
+
+  dist.hidePropertiesIn(defaultProperties, values, propsToHide);
+  return defaultProperties;
+}
+
+// /**
+//  * @param {Object} values
+//  * @returns {Problem[]} returns a list of problems.
+//  */
+// export function check(values) {
+//    /** @type {Problem[]} */
+//    const errors = [];
+//    // Add errors to the above array to throw errors in Studio and Studio Pro.
+//    /* Example
+//    if (values.myProperty !== "custom") {
+//        errors.push({
+//            property: `myProperty`,
+//            message: `The value of 'myProperty' is different of 'custom'.`,
+//            url: "https://github.com/myrepo/mywidget"
+//        });
+//    }
+//    */
+//    return errors;
+// }
+
+// /**
+//  * @param {object} values
+//  * @param {boolean} isDarkMode
+//  * @returns {object}
+//  */
+// export function getPreview(values, isDarkMode) {
+//     // Customize your pluggable widget appearance for Studio Pro.
+//     return {
+//         type: "Container",
+//         children: []
+//     };
+// }
+
+// /**
+//  * @param {Object} values
+//  * @param {("web"|"desktop")} platform
+//  * @returns {string}
+//  */
+// export function getCustomCaption(values, platform) {
+//     return "JavaScriptSnippet";
+// }
+
+exports.getProperties = getProperties;
--- src/PWAOverlayForiOS.jsx
+++ src/PWAOverlayForiOS.jsx
@@ -1,13 +1,25 @@
 import "./ui/PWAOverlayForiOS.css";
-import { createElement, useEffect } from "react";
+import { createElement, useEffect, useState } from "react";
 import shareIcon from "./shareIcon.svg";
 
-export default function PWAOverlayForiOS({ infoText, instructionTextStart, instructionTextEnd }) {
+export default function PWAOverlayForiOS({
+    instructionCanBeClosed,
+    instructionClosed,
+    infoText,
+    instructionTextStart,
+    instructionTextEnd
+}) {
     const isIos = () => {
         const userAgent = window.navigator.userAgent.toLowerCase();
         return /iphone|ipad|ipod/.test(userAgent);
     };
     const isInStandaloneMode = () => "standalone" in window.navigator && window.navigator.standalone; // Detects if device is in standalone mode
+    const [hideOverlay, setHideOverlay] = useState(null);
+
+    function handleClick() {
+        setHideOverlay(true);
+        instructionClosed.setValue(true);
+    }
 
     useEffect(() => {
         if (document.documentElement.classList) {
@@ -20,14 +32,37 @@ export default function PWAOverlayForiOS({ infoText, instructionTextStart, instr
         }
     });
 
-    return (
-        <div className="ios-pwa">
-            <p className="ios-pwa__text">{infoText.value}</p>
-            <div className="ios-pwa__bottom">
-                <span>{instructionTextStart.value}</span>
-                <img className="ios-pwa__icon" src={shareIcon} alt="share icon" />
-                <span>{instructionTextEnd.value}</span>
+    useEffect(() => {
+        if (instructionClosed && instructionClosed === true) {
+            setHideOverlay(true);
+        } else if (instructionClosed && instructionClosed === false) {
+            setHideOverlay(false);
+        }
+    });
+
+    if (hideOverlay && hideOverlay === true) {
+        return <div className="ios-pwa"></div>;
+    } else {
+        return (
+            <div className="ios-pwa">
+                {console.info({ hideOverlay })}
+                <p className="ios-pwa__text">{infoText.value}</p>
+                <div className="ios-pwa__bottom">
+                    <span>{instructionTextStart.value}</span>
+                    <img className="ios-pwa__icon" src={shareIcon} alt="share icon" />
+                    <span>{instructionTextEnd.value}</span>
+                </div>
+                {instructionCanBeClosed && (
+                    <a className="ios-pwa__close" onClick={handleClick}>
+                        <svg viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                fill="#5D5D5D"
+                                d="M9.78807 11.0466L6 7.25855L2.21193 11.0466C1.79524 11.4633 1.11339 11.4633 0.696699 11.0466C0.280011 10.6299 0.280012 9.94808 0.696699 9.53139L4.48477 5.74332L0.696699 1.95525C0.280012 1.53856 0.280011 0.856708 0.696699 0.44002C1.11339 0.0233315 1.79524 0.0233315 2.21193 0.440019L6 4.22809L9.78807 0.440019C10.2048 0.0233315 10.8866 0.0233315 11.3033 0.44002C11.72 0.856708 11.72 1.53856 11.3033 1.95525L7.51523 5.74332L11.3033 9.53139C11.72 9.94808 11.72 10.6299 11.3033 11.0466C10.8866 11.4633 10.2048 11.4633 9.78807 11.0466Z"
+                            />
+                        </svg>
+                    </a>
+                )}
             </div>
-        </div>
-    );
+        );
+    }
 }
